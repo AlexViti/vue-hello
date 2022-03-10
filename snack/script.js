dcafe8777933c3d0ec8231b9c57ab481ff78b020
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-03-10 18:07:14 +0100

    Add snack 3

--- snack/script.js
+++ snack/script.js
@@ -23,4 +23,32 @@ const animals = [
 ];
 
 const mammals = animals.filter(animal => animal.classe == 'mammiferi');
-console.log(mammals);
+console.log('Snack 2: ', mammals);
+
+/*
+Snack 3:
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un’età.
+Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.
+ */
+
+const persons = [
+	{
+		name: 'Jack',
+		surname: 'London',
+		age: 40
+	},
+	{
+		name: 'Kurt',
+		surname: 'Cobain',
+		age: 27
+	},
+	{
+		name: 'Anne',
+		surname: 'Frank',
+		age: 16
+	}
+];
+
+const persons2 = persons.map(person => person.message = `${person.name} ${person.surname} ${person.age < 18 ? 'cannot': 'can'} drive`);
+console.log(persons, persons2);

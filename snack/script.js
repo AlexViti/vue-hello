6f1de18e8403827142ea6acf0db1adc1bc3ae32a
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-03-10 17:54:22 +0100

    Add snack 2

--- snack/script.js
+++ snack/script.js
@@ -8,3 +8,19 @@ A partire da un array di stringhe, crea un secondo array formattando le stringhe
 const arr = ['meno', 'mAlE', 'CHE', 'SilVio', "c'è"];
 const arr2 = arr.map(str => str[0].toUpperCase() + str.substring(1).toLowerCase());
 console.log('Snack 1: ', arr, arr2);
+
+/*
+Snack 2:
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Crea un nuovo array con la lista dei mammiferi.
+*/
+
+const animals = [
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+];
+
+const mammals = animals.filter(animal => animal.classe == 'mammiferi');
+console.log(mammals);
